27e21a6d4970fe13322545e8c2de6e3566c47509
zolakulani <shikwenizolakulani@gmail.com>
2024-05-19 00:56:21 -0700

    Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import models
 import uuid
 from datetime import datetime
 
@@ -24,6 +25,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         """
@@ -34,8 +36,10 @@ class BaseModel:
     def save(self):
         """
         Updates the public instance attribute 'updated_at' with the current datetime.
+        Calls save() method of storage.
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+import json
+import os
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Returns the dictionary __objects.
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        Sets in __objects the obj with key <obj class name>.id.
+        """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """
+        Serializes __objects to the JSON file (path: __file_path).
+        """
+        obj_dict = {}
+        for key, obj in self.__objects.items():
+            obj_dict[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        """
+        Deserializes the JSON file to __objects (only if the file exists).
+        """
+        if exists(self.__file_path):
+            with open(self.__file_path, 'r') as file:
+                obj_dict = json.load(file)
+                for key, value in obj_dict.items():
+                    class_name, obj_id = key.split('.')
+                    cls = globals()[class_name]
+                    self.__objects[key] = cls(**value)
+
